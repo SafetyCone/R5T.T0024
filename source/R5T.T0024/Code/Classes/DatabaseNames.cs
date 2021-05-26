d6ca2967f11a1c672ce570a493637022233c860d
David Coats <david@getsafetycone.com>
2021-05-26 15:45:12 -0700

    Add IDatabaseNames and implementation.

---
+++ source/R5T.T0024/Code/Classes/DatabaseNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.T0024
+{
+    public class DatabaseNames : IDatabaseNames
+    {
+        #region Static
+
+        public static DatabaseNames Instance { get; } = new DatabaseNames();
+
+        #endregion
+    }
+}
---
+++ source/R5T.T0024/Code/Interfaces/IDatabaseNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.T0024
+{
+    /// <summary>
+    /// Empty interface for use as an extension methods base.
+    /// </summary>
+    public interface IDatabaseNames
+    {
+    }
+}
